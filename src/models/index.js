04e8dcfcad59a608fadce4d57590e20f12ffc5d2
van <vannb170801@gmail.com>
2022-11-07 11:15:54 +0700

    develop

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,37 @@
 'use strict';
-
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+//const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+const custommizeconfig = {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  custommizeconfig);
+/*
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+*/
 
 fs
   .readdirSync(__dirname)
